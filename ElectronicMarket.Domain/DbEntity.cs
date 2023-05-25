0844e77f4b4622f2f0e7082ad2858770dbe725be
Katy248 <petrovanton247@gmail.com>
2023-05-25 19:40:26 +0300

    Initial commit

---
+++ ElectronicMarket.Domain/DbEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicMarket.Domain;
+
+public abstract class DbEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+}
