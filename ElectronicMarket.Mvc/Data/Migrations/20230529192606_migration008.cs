c860dfb0b1ed98cff046731536a81de451f04e0b
Армен <armen.sargsyan2003@mail.ru>
2023-05-29 23:04:24 +0300

    Небольшие дизайнерские изменения

---
+++ ElectronicMarket.Mvc/Data/Migrations/20230529192606_migration008.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElectronicMarket.Mvc.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration008 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "Carts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "Carts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
